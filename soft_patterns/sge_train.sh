e060374be80d993c4950fc3de2ba58035fc10de2
Ronald <roncardenasacosta@gmail.com>
2019-05-16 09:34:50 +0200

    report

--- soft_patterns/sge_train.sh
+++ soft_patterns/sge_train.sh
@@ -27,3 +27,49 @@ qsub -q 'gpu*' -cwd -l gpu=1,gpu_cc_min3.5=1,gpu_ram=2G,mem_free=10G,act_mem_fre
 -o $HOME/sent-anlz-lct/soft_patterns/models/bootstrap/log.out \
 -e $HOME/sent-anlz-lct/soft_patterns/models/bootstrap/log.err \
 wraps/train_bst.sh
+
+
+# local lines
+
+python3 soft_patterns_test.py \
+-p 6-10_5-10_4-10 \
+--vd amazon_reviews/test_src.data \
+--vl amazon_reviews/test_src.labels \
+--batch_size 20 \
+--mlp_hidden_dim 150 \
+-e ../emb/glove.840B.300d.txt \
+--input_model models/movies-src/model_9.pth \
+--gpu
+
+python3 interpret_classification_results.py \
+-p 6-10_5-10_4-10 \
+--vd amazon_reviews/dev_tgt.data \
+--vl amazon_reviews/dev_tgt.labels \
+-e ../emb/glove.840B.300d.txt \
+--batch_size 20 \
+--mlp_hidden_dim 150 \
+--input_model models/movies-src/model_9.pth \
+--ofile models/movies-src/analisis.log
+
+
+
+python3 interpret_classification_results.py \
+-p 6-10_5-10_4-10 \
+--vd amazon_reviews/dev_tgt.data \
+--vl amazon_reviews/dev_tgt.labels \
+-e ../emb/glove.840B.300d.txt \
+--batch_size 20 \
+--mlp_hidden_dim 150 \
+--input_model models/bootstrap/bst_model_2_1.pth \
+--ofile models/bootstrap/analisis.log
+
+python3 visualize.py \
+-p 6-10_5-10_4-10 \
+--vd amazon_reviews/dev_tgt.data \
+--vl amazon_reviews/dev_tgt.labels \
+-e ../emb/glove.840B.300d.txt \
+--batch_size 20 \
+--mlp_hidden_dim 150 \
+--input_model models/bootstrap/bst_model_2_1.pth \
+--k_best 5
+
